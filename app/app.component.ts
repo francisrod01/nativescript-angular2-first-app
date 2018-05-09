8da5eb9cfb8f45d829fced92fda743f0b4d34775
Francis Rodrigues <francis.rod01@outlook.com>
2018-05-09 17:15:15 -0300

    Added a simple Image component to the template

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from "@angular/core";
   template: `
     <ActionBar title="My App" class="action-bar"></ActionBar>
     <!-- Your UI components go here -->
+    <Image src="~/images/apple.jpg"></Image>
   `
 })
 export class AppComponent {
